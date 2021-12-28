914ae19d2274cd6d7eb5a40b4a3bca907866c39d
ChloeKonde <konde.chloe@gmail.com>
2021-12-29 02:34:40 +0500

    test

--- binaryTree/hashes.js
+++ binaryTree/hashes.js
@@ -1,139 +1,118 @@
-const fs = require('fs');
+const fs = require("fs");
 
-var md5 = require('md5');
+const md5 = require("md5");
 
-class Node 
-{
-    constructor(data, offset = null) 
-    {
-        this.left = null;
-        this.right = null;
-        this.data = data;
-        this.offset = offset;
-    }
+class Node {
+  constructor(data, offset = null) {
+    this.left = null;
+    this.right = null;
+    this.data = data;
+    this.offset = offset;
+  }
 }
 
 class Tree {
-    constructor() 
-    {
-        this.root = null;  
-    }
-  
-    insert(data, newOffset) 
-    {
+  constructor() {
+    this.root = null;
+  }
 
-    var newNode = new Node(data, newOffset);
+  insert(data, newOffset) {
+    const newNode = new Node(data, newOffset);
 
-    if(this.root === null){
-        this.root = newNode;
-        this.offset = newOffset;
-    }
-    else
-        this.insertNode(this.root, newNode, newOffset);
-    }
+    if (this.root === null) {
+      this.root = newNode;
+      this.offset = newOffset;
+    } else this.insertNode(this.root, newNode, newOffset);
+  }
 
-    insertNode(node, newNode, newOffset) 
-    {
-        if(newNode.data < node.data)
-        {
-            if(node.left == null)
-            {
-                node.left = newNode;
-                node.offset = newOffset;
-            }
-            else
-                this.insertNode(node.left, newNode, newOffset);
-        }
-        else
-        {
-            if(node.right == null)
-            {
-                node.right = newNode;
-                node.offset = newOffset;
-            }
-            else
-                this.insertNode(node.right, newNode, newOffset);
-        }
-    }
+  insertNode(node, newNode, newOffset) {
+    if (newNode.data < node.data) {
+      if (node.left == null) {
+        node.left = newNode;
+        node.offset = newOffset;
+      } else this.insertNode(node.left, newNode, newOffset);
+    } else if (node.right == null) {
+      node.right = newNode;
+      node.offset = newOffset;
+    } else this.insertNode(node.right, newNode, newOffset);
+  }
 
-    search(root, key) 
-    {
-        if (root === null || root.data == key)
-        {
-            return `Hash ${root.data} has offset - ${root.offset}`;
-        }
-        if (root.data < key)
-        {
-            return this.search(root.right, key);
-        }
-        return this.search(root.left, key);
+  search(root, key) {
+    if (root === null || root.data == key) {
+      return `Hash ${root.data} has offset - ${root.offset}`;
     }
+    if (root.data < key) {
+      return this.search(root.right, key);
+    }
+    return this.search(root.left, key);
+  }
 }
 
-function generateHashes(length)
-{
-    var result = '';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-';
-    var charactersLength = characters.length;
-   
-    for ( let i = 0; i < length; i++ ) 
-    {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    
-    return result;
+function generateHashes(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
 
-let hashTree = new Tree();
-var tempTree = [];
+const hashTree = new Tree();
+const tempTree = [];
 
-var stream = fs.createWriteStream("data.txt");
-        
-stream.on('open', function(fd) 
-{
-    for ( let i = 0; i < 10000; i++ )
-    {
-        let hashes = generateHashes(36);
-        simpleHashes = md5(hashes).substr(0,7);
-        tempTree.push(simpleHashes);
-        stream.write(`${hashes} - ${simpleHashes}\n`);
-    }
+const stream = fs.createWriteStream("data.txt");
 
-    hashTree.root = new Node(tempTree[0], 1);
-    tempTree.pop(0);
+stream.on("open", function (fd) {
+  for (let i = 0; i < 10000; i++) {
+    const hashes = generateHashes(36);
+    simpleHashes = md5(hashes).substr(0, 7);
+    tempTree.push(simpleHashes);
+    stream.write(`${hashes} - ${simpleHashes}\n`);
+  }
 
-    for (let i = 0; i < tempTree.length; i++) 
-    {
-        hashTree.insert(tempTree[i], i + 2);
-    }
+  hashTree.root = new Node(tempTree[0], 1);
+  tempTree.pop(0);
 
-    let hs = [];
+  for (let i = 0; i < tempTree.length; i++) {
+    hashTree.insert(tempTree[i], i + 2);
+  }
 
-    for ( let i = 0; i < 1000; i++ ) 
-    {
-        randomHash = Math.floor(Math.random() * 4);
-        hs.push(tempTree[randomHash]);
-    }
- 
-    stream.end(); 
- 
-    //search in the tree
-    console.time('Execution Time');
-    for (let i = 0; i < 1000; i++) 
-    {
-        hashTree.search(hashTree.root, hs[i]);
-    }
-    console.timeEnd('Execution Time');
+  const hs = [];
 
-    stream.on('finish', () => {
-        let findings = 0;
-        //search in the file after saving file
-        console.time('Execution Time');
-        fs.readFileSync("data.txt").toString().split("\n").forEach(function(line, index, arr) {
-            if (index === arr.length - 1 && line === "") { return; }
-            if (line.includes(hs[index]))
-                findings++; //used for checking that this works
-        });
-            console.timeEnd('Execution Time'); 
-    }); 
-}); 
+  for (let i = 0; i < 1000; i++) {
+    randomHash = Math.floor(Math.random() * 4);
+    hs.push(tempTree[randomHash]);
+  }
+
+  stream.end();
+
+  // search in the tree
+  console.time("Execution Time");
+  for (let i = 0; i < 1000; i++) 
+  {
+    hashTree.search(hashTree.root, hs[i]);
+  }
+  console.timeEnd("Execution Time");
+
+  stream.on("finish", () => {
+    let findings = 0;
+    // search in the file after saving file
+    console.time("Execution Time");
+    fs.readFileSync("data.txt")
+      .toString()
+      .split("\n")
+      .forEach(function (line, index, arr) 
+      {
+        if (index === arr.length - 1 && line === "") 
+        {
+          return;
+        }
+        if (line.includes(hs[index])) findings++; // used for checking that this works
+      });
+    console.timeEnd("Execution Time");
+  });
+});
