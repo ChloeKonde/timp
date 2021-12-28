878e698c9a3ca1c714cd4eabd540777c7778bd8f
ChloeKonde <konde.chloe@gmail.com>
2021-12-29 01:50:07 +0500

    test

--- binaryTree/hashes.js
+++ binaryTree/hashes.js
@@ -125,20 +125,15 @@ stream.on('open', function(fd)
     }
     console.timeEnd('Execution Time');
 
-    stream.on('finish', () => {  
-        let findings = 0;   
-        //search in the file after saving file  
-        console.time('Execution Time'); 
-        fs.readFileSync("data.txt").toString().split("\n").forEach(function(line, index, arr) {  
-            if (index === arr.length - 1 && line === "") { return; } 
-            if (line.includes(hs[index])) 
-                findings++; //used for checking that this works 
-        }); 
+    stream.on('finish', () => {
+        let findings = 0;
+        //search in the file after saving file
+        console.time('Execution Time');
+        fs.readFileSync("data.txt").toString().split("\n").forEach(function(line, index, arr) {
+            if (index === arr.length - 1 && line === "") { return; }
+            if (line.includes(hs[index]))
+                findings++; //used for checking that this works
+        });
             console.timeEnd('Execution Time'); 
-    });   
-});
-
- 
- 
-     
- 
+    }); 
+}); 
